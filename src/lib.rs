b860e600ea1a43b0bb47e81b8f9a2ef223338e56
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-05-04 14:09:12 +0200

    feat: add div function

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@ pub fn sum(a: i32, b: i32) -> i32 {
     a + b
 }
 
+pub fn div(a: i32, b: i32) -> i32 {
+    a / b
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -9,5 +13,10 @@ mod tests {
     #[test]
     fn test_sum() {
         assert_eq!(sum(1, 2), 3);
+    }
+
+    #[test]
+    fn test_div() {
+        assert_eq!(div(4, 2), 2);
     }
 }
