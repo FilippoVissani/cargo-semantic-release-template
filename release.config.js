c415c68fb71afc588d2af0b3182d12ba407c0187
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-05-04 22:04:25 +0200

    fix: fix Semantic release

--- release.config.js
+++ release.config.js
@@ -1,6 +1,5 @@
 const config = require('semantic-release-preconfigured-conventional-commits')
 const publishCommands = `
-cargo login \${CARGO_REGISTRY_TOKEN} || exit 1
 sed -i 's/version = "\${lastRelease.version}"/version = "\${nextRelease.version}"/' Cargo.toml || exit 2
 git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md || exit 3
 git push --force origin \${nextRelease.version} || exit 4
