1c2b591f3141dc28ad90ef61bde7e54fd9c59c4e
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-05-07 17:57:13 +0200

    ci: Semantic release

--- release.config.js
+++ release.config.js
@@ -1,4 +1,10 @@
 const config = require('semantic-release-preconfigured-conventional-commits')
+const verifyConditionsCommands = `
+sed -i 's/version.*/version = "\${nextRelease.version}"/g' Cargo.toml || exit 1
+git add -A || exit 2
+git commit -m "chore(release)!: [skip ci] \${nextRelease.version} released" || exit 3
+cargo publish --dry-run || exit 4
+`
 const prepareCommands = `
 sed -i 's/version.*/version = "\${nextRelease.version}"/g' Cargo.toml || exit 1
 git add -A || exit 2
@@ -6,10 +12,10 @@ git commit -m "chore(release)!: [skip ci] \${nextRelease.version} released" || e
 git push --force origin || exit 4
 `
 const publishCommands = `
-git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md || exit 5
-git push --force origin \${nextRelease.version} || exit 6
-cargo package || exit 7
-cargo publish || exit 8
+git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md || exit 1
+git push --force origin \${nextRelease.version} || exit 2
+cargo package || exit 3
+cargo publish || exit 4
 `
 const releaseBranches = ["main"]
 config.branches = releaseBranches
